5e3b73977f4a922e566b1ccb7ba82e8661293927
Gavin Johnson <gajohnson@twilio.com>
2023-04-04 17:53:52 +0100

    mondrian is more clever: look no inputs

--- mondrian.sh
+++ mondrian.sh
@@ -5,38 +5,46 @@
 # post on social media and measure popularity (occasionally sneak in a curated recurrink?)
 # use ranking to improve RANDOM
 
-model=$1
-if [ ! -f "SVG/${model}.svg" ]
-then
-  echo "File not found!"
-  exit 1
-fi
-
-cells=$(./recurrink.py -c ${model})
-if [ -z "$cells" ]
-then
-  echo "cannot find cells for ${model}"
-  exit 1
-fi
-
-# create SVG 
-
-# ./input.py 
-# --output SVG/z.svg
-#  --scale SCALE    Scale in or out (0.5 - 2.0)  factor [0.5, 1.0, 2.0]
-#  z.rink
-
-# TODO these are supposed to be updated interactively :(
+# TODO these are supposed to be updated interactively : need to update effect.py
 # 'fill': '#fff',
 # 'fill_opacity':1.0,
 # 'stroke':'#000',
 # 'stroke_dasharray': 0,
 # 'stroke_opacity':1.0,
 
+model_path=$(ls -1 models/*.rink| sort -R| tail -1)
+model=${model_path%.*}  # retain the part before the colon
+model=${model##*/}  # retain the part after the last slash
+echo $model
+
+# do a RINK test
+if [ ! -f ${model_path} ]
+then
+  echo "File '$model_path' not found! Run recurrink.py -a"
+  exit 1
+fi
+
+cells=$(./recurrink.py -c ${model})
+if [ -z "${cells}" ]
+then
+  echo "cannot find cells for ${model}"
+  exit 1
+fi
+
 get_random_index() {
   i=$((0 + $RANDOM % $1))
   return $i
 }
+
+factor() {
+  declare -a a=(0.5 1.0 2.0)
+  get_random_index 3
+  scale=${a[$?]}
+}
+
+factor 1
+# create SVG from RINK
+./input.py --output /tmp/$model.svg --scale $scale $model_path
 
 # default 'shape':'square',
 shapes() {
@@ -79,11 +87,11 @@ background_colour() {
 }
 
 # update cell attributes
+echo "updating $model"
 for cell in ${cells}
 do
-  # default 'stroke_width': 0
   get_random_index 10
-  width=$?
+  width=$?  # default 'stroke_width': 0
 
   # pretend to pass arg in order to fool the syntax highlighter
   shapes 1
@@ -93,15 +101,21 @@ do
   background_colour 1
 
   # update cells
-  echo "${cell} ${model}"
   echo "${cell},${model},${shape},${width},${facing},${size},${bg},${top}" >>mondrian.log
+  data="${data},${cell},${model},${shape},${width},${facing},${size},${bg},${top}"
+  echo -n "$cell" 
 ./effect.py \
    --id ${cell}1 \
-   --output SVG/${model}.svg \
+   --output /tmp/${model}.svg \
    --shape ${shape} \
    --width ${width} \
    --facing ${facing} \
    --size ${size} \
    --bg ${bg} \
-   --top ${top} SVG/${model}.svg
+   --top ${top} /tmp/${model}.svg
 done
+
+digest=$(./recurrink.py -m ${model} -d ${data})
+/bin/mv "/tmp/${model}.svg" "SVG/${digest}.svg"
+echo "drew new recurrink in SVG"
+echo "${digest}.svg"
