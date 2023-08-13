cc50f777257e45e6a126c94138aaab475450fd2f
GAVIN CRAIG JOHNSON <gavincraig.johnson@vodafone.com>
2023-08-13 22:42:38 +0200

    changed blocks table to improve symmetry

--- sql/timpani.sql
+++ sql/timpani.sql
@@ -1,37 +1,33 @@
 -- timpani
--- 2 way diagonal, cell symmetry without sexagesimal
--- a - l: facing n s e w
--- m: facing all
-
--- c a b c
--- a b a b
--- b a b a
--- c b a c
-
+-- 1 way diagonal, cell symmetry without sexagesimal
+-- a - d: facing n s e w 	g - k: superimposed	e - f: facing all
+INSERT INTO models (model, uniqcells, blocksizeXY, scale) 
+VALUES ('timpani', 11, '{4, 4}', 1.0);
 -- * / \ *
 -- / \ / \
 -- \ / \ /
 -- * \ / *
 
-
-INSERT INTO models (model, uniqcells, blocksizeXY, scale) 
-VALUES ('timpani', 3, '{4, 4}', 1.0);
+-- f a c f 	. g i .
+-- a c e d 	g i k j
+-- c e d b 	i k j h
+-- f d b f 	. j h .
 
 INSERT INTO blocks (model, position, cell, top) 
 VALUES 
-  ('timpani', '{ 0, 0 }', 'c', null),
-  ('timpani', '{ 1, 0 }', 'a', 'd'),
-  ('timpani', '{ 2, 0 }', 'b', 'e'),
-  ('timpani', '{ 3, 0 }', 'c', null),
-  ('timpani', '{ 0, 1 }', 'a', 'd'),
-  ('timpani', '{ 1, 1 }', 'b', 'e'),
-  ('timpani', '{ 2, 1 }', 'a', 'd'),
-  ('timpani', '{ 3, 1 }', 'b', 'e'),
-  ('timpani', '{ 0, 2 }', 'b', 'e'),
-  ('timpani', '{ 1, 2 }', 'a', 'd'),
-  ('timpani', '{ 2, 2 }', 'b', 'e'),
-  ('timpani', '{ 3, 2 }', 'a', 'd'),
-  ('timpani', '{ 0, 3 }', 'c', null),
-  ('timpani', '{ 1, 3 }', 'b', 'e'),
-  ('timpani', '{ 2, 3 }', 'a', 'd'),
-  ('timpani', '{ 3, 3 }', 'c', null);
+  ('timpani', '{ 0, 0 }', 'f', null),
+  ('timpani', '{ 1, 0 }', 'a', 'g'),
+  ('timpani', '{ 2, 0 }', 'c', 'i'),
+  ('timpani', '{ 3, 0 }', 'f', null),
+  ('timpani', '{ 0, 1 }', 'a', 'g'),
+  ('timpani', '{ 1, 1 }', 'c', 'i'),
+  ('timpani', '{ 2, 1 }', 'e', 'k'),
+  ('timpani', '{ 3, 1 }', 'd', 'j'),
+  ('timpani', '{ 0, 2 }', 'c', 'i'),
+  ('timpani', '{ 1, 2 }', 'e', 'k'),
+  ('timpani', '{ 2, 2 }', 'd', 'j'),
+  ('timpani', '{ 3, 2 }', 'b', 'h'),
+  ('timpani', '{ 0, 3 }', 'f', null),
+  ('timpani', '{ 1, 3 }', 'd', 'j'),
+  ('timpani', '{ 2, 3 }', 'b', 'h'),
+  ('timpani', '{ 3, 3 }', 'f', null);
