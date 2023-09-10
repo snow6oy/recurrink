544ea5ff834d616c520611018917d0bf904ffd9b
snow6oy <snow6oy@gmail.com>
2023-09-10 21:31:46 +0200

    add missing shapes

---
+++ sql/line.sql
@@ -0,0 +1,9 @@
+INSERT INTO geometry (gid, shape, size, facing, top) 
+VALUES 
+  (26 ,'line','small','south', TRUE),
+  (29 ,'line','small','west', TRUE),
+  (30 ,'line','small','west', FALSE),
+  (31 ,'line','large','west', FALSE);
+
+-- the following are ANNOYING gaps that i may fix one day
+-- 38 39 40 51 52 58 59 60 61 62 63 64 65 66 67 68 69
