f1621dafc22a8c6bafb5794470f2d9753bf47416
snow6oy <4960142+snow6oy@users.noreply.github.com>
2024-08-15 13:24:42 +0200

    prototype algorithm to compare cells

--- scripts/cmp_once.py
+++ scripts/cmp_once.py
@@ -1,17 +1,36 @@
-shapes = list()
+'''
+transform lower char upper
+insert any number
+compare transformed versions
+'''
 
-def backgroundCheck(bg, a):
+def mergeBackground(bg, a):
   ''' compare shapes from different layers 
       merge if they overlap
   '''
-  [shapes.append(b.upper()) for b in bg if a ==  b]
+  found = None
+  for i, b in enumerate(bg):
+    if a ==  b:
+      bg[i] = b.upper() 
+    elif a == '1':
+      found = 'one'
+  # better to concatenate two lists
+  bg.append(found) if found else None
+  return bg
 
-def cmpOnce(doc):
+def makeFlat(doc):
   ''' loop a nested list so that each item
       from the second list onwards is compared 
       to the first list exactly once
   '''
-  [[backgroundCheck(doc[0], cell) for cell in up] for up in doc[1:]] # fg and top group
-doc = [ list('abc'), list('ayz'), list('xbz') ]
-cmpOnce(doc)
-print(shapes)
+  bg = doc.pop(0)
+  for up in doc: # fg and top group
+    for cell in up:
+      bg = mergeBackground(bg, cell) 
+  return bg
+
+doc = [ list('abc'), list('ayzC'), list('bB1') ]
+flat_doc = makeFlat(doc)
+print(doc)
+print(flat_doc)
+
