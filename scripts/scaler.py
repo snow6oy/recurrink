9830272e32ebeac4555c684553b172da139c4852
snow6oy <4960142+snow6oy@users.noreply.github.com>
2024-08-25 15:56:34 +0200

    720 is the new 1080

--- scripts/scaler.py
+++ scripts/scaler.py
@@ -104,10 +104,10 @@ if __name__ == '__main__':
                    gridsz cellsz maxmm
     a4 210x297 mm     180     24   210
     a3 297x420 mm     270     36   297
-    a3                702    118   980  above * 3.3 
+    a3                720     60   980  above * 3.3 
   '''
   scale_range = [0.5, 1.0, 1.5, 2.0]
-  test_vals = [ (1080, 60, 1081, 5), (900, 60, 980, 0) ]
+  test_vals = [ (1080, 60, 1081, 5), (720, 60, 980, 0) ]
   # (270, 36, 297, 0) ]
 
   blocksize = 24  # afroclave[x] = 24
