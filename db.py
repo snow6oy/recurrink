babf42e4d63f05f8ae103b41acbddc8445e7276a
snow6oy <4960142+snow6oy@users.noreply.github.com>
2024-09-09 19:46:29 +0200

    port 5433

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
 import psycopg2
+# port 5433 is tmp during transition from postgres 14 to 16
 
 class Db:
 
   def __init__(self):
     ''' create connection to postgres
     '''
-    connection = psycopg2.connect(dbname='recurrink')
-    connection.autocommit = True  # Ensure data is added to the database immediately after write commands
+    connection = psycopg2.connect(dbname='recurrink', port=5433)
+    connection.autocommit = True
     self.cursor = connection.cursor()
