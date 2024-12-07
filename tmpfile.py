f64486a6e3756ba73ce4fb5265e08fb4ec2b5bae
snow6oy <4960142+snow6oy@users.noreply.github.com>
2024-12-07 15:57:01 +0100

    fix depth in tmp

--- tmpfile.py
+++ tmpfile.py
@@ -121,7 +121,7 @@ class TmpFile():
         return None
       else: # read link
         path = os.readlink(path)
-        model = re.findall(r"[a-z0-9]+", path)[1]
+        model = re.findall(r"[a-z0-9]+", path)[2]
         return model, link
     elif len(links) == 0 and model and ver: # first time
       os.symlink(f'/tmp/recurrink/{model}.txt', f'/tmp/recurrink/{ver}')
