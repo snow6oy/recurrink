cb90b122b0692d5e3ef834ae308b249733d56a63
snow6oy <4960142+snow6oy@users.noreply.github.com>
2025-02-10 22:00:49 +0100

    colours for gthumb

--- config.py
+++ config.py
@@ -2,24 +2,24 @@
 class config:
   cells = {
     'a': {
-      'bg': '#CCC', 'fill': '#FFF', 'fill_opacity': 1.0,
+      'bg': '#F00', 'fill': '#FF0', 'fill_opacity': 1.0,
       'shape': 'square', 'facing': 'all', 'size': 'medium', 'top': False,
-      'stroke': '#000', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
+      'stroke': '#F00', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
     },
     'b': {
-      'bg': '#CCC', 'fill': '#000', 'fill_opacity': 1.0,
+      'bg': '#F00', 'fill': '#00F', 'fill_opacity': 1.0,
       'shape': 'line', 'facing': 'north', 'size': 'medium', 'top': False,
-      'stroke': '#000', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
+      'stroke': '#F00', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
     },
     'c': {
-      'bg': '#CCC', 'fill': '#000', 'fill_opacity': 1.0,
+      'bg': '#F00', 'fill': '#00F', 'fill_opacity': 1.0,
       'shape': 'square', 'facing': 'all', 'size': 'small', 'top': True,
-      'stroke': '#000', 'stroke_dasharray': 0, 'stroke_opacity': 1.0, 'stroke_width': 0, 
+      'stroke': '#F00', 'stroke_dasharray': 0, 'stroke_opacity': 1.0, 'stroke_width': 0, 
     },
     'd': {
-      'bg': '#CCC', 'fill': '#FFF', 'fill_opacity': 1.0,
+      'bg': '#F00', 'fill': '#FF0', 'fill_opacity': 1.0,
       'shape': 'line', 'facing': 'east', 'size': 'large', 'top': True,
-      'stroke': '#000', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
+      'stroke': '#F00', 'stroke_dasharray': 0, 'stroke_opacity': 0, 'stroke_width': 0, 
     }
   }
   positions = {
