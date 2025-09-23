a243910c493c0f40e36f12b1f77e6a8904132b1c
Gavin Johnson <4960142+snow6oy@users.noreply.github.com>
2025-09-23 22:29:11 +0200

    tests how palette files import/export

--- t/tmpfile.py
+++ t/tmpfile.py
@@ -11,19 +11,26 @@ class Test(unittest.TestCase):
   def setUp(self):
     self.tf = TmpFile()
     self.model = 'minkscape'
+    self.paldata = (
+      ['#7ac465','0.5','#f37022'],
+      ['#7ac465','0.5','#e6aace'],
+      ['#7ac465','0.5','#cfe8d3'],
+      ['#7ac465','0.5','#fddac4'],
+      ['#7ac465','0.5','#8754a1']
+    )
 
   def test_a(self):
     ''' create seed from minkscape YAML
     '''
     #self.pp.pprint(config.cells)
     seed = self.tf.digestString(config.cells)
-    self.assertEqual(155, len(seed))
+    self.assertEqual(144, len(seed))
 
   def test_b(self):
     ''' make digest
     '''
     self.tf.setDigest(celldata=config.cells)
-    self.assertEqual('ad2fdea8eac5f642c35d780ba678f8e5', self.tf.digest)
+    self.assertEqual('265550a81314a36daa05b5ed2d81ee1d', self.tf.digest)
 
   def test_c(self):
     ''' Read celldata only, no meta stuff '''
@@ -36,6 +43,17 @@ class Test(unittest.TestCase):
     ''' read the meta stuff'''
     metadata = self.tf.readConf(self.model, meta=True)
     self.assertEqual(3, len(metadata.keys()))
+
+  def test_e(self):
+    self.tf.exportPalfile(self.id(), self.paldata)
+    self.assertTrue(os.path.isfile(f'palettes/{self.id()}.txt'))
+
+  def test_f(self):
+    ''' run test_e first
+    '''
+    pal = self.tf.importPalfile('tmpfile_test_e')
+    self.assertEqual(5, len(pal))
+  
 
 '''
 the 
