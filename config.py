0e83f4b559bc7e4882409a85689ca78525e99b33
Gavin Johnson <4960142+snow6oy@users.noreply.github.com>
2025-09-24 22:21:33 +0200

    merged db into config

--- config.py
+++ config.py
@@ -1,5 +1,22 @@
-# minkscape
+import psycopg2
+
+class Db:
+
+  def __init__(self):
+    ''' create connection to postgres
+    '''
+    connection = psycopg2.connect(dbname='recurrink') #, port=5433)
+    connection.autocommit = True
+    self.cursor = connection.cursor()
+
 class config:
+  directory = {
+       'rinks': '/Users/gavin/Dropbox/familia/rinks',
+        'pubq': '/Users/gavin/Pictures/pubq',
+    'palettes': '/Users/gavin/Library/Application Support/org.inkscape.Inkscape/config/inkscape/palettes'
+  }
+
+
   cells = {
     'a': {
       'color': {
@@ -102,13 +119,6 @@ class config:
   penam = [{}, {}, {
     '#F00': 'red', '#090': 'green', '#00F': 'blue'
   }]
-  directory = {
-       'rinks': '/Users/gavin/Dropbox/familia/rinks',
-        'pubq': '/Users/gavin/Pictures/pubq',
-    'palettes': '/Users/gavin/Library/Application Support/org.inkscape.Inkscape/config/inkscape/palettes'
-  }
-
-
 '''
 the
 end 
