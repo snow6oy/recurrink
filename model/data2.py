9b3ae9e25937a2206ae02919713e7c7801e79761
snow6oy <snow6oy@gmail.com>
2026-01-16 22:11:31 +0100

    refactored dbmigrator to model/data2

---
+++ model/data2.py
@@ -0,0 +1,167 @@
+import pprint
+import psycopg2
+from cell.transform import Transform
+
+class ModelData2(Transform):
+  ''' access to models, blocks, compass and pens table
+  '''
+  def __init__(self):
+    self.pp     = pprint.PrettyPrinter(indent=2)
+    super().__init__()
+
+  def model(self, name=None, mid=0):
+    ''' retrieve or create and retrieve a model id from a name
+        OR given a model ID, retrieve a name
+        given neither, return a list of models
+    '''
+    new_record_count = 0
+    models           = self.modelRead()
+    if name in models: found = models.index(name) # ID or 0 if name is None
+    else:              found = False
+
+    if found:  return new_record_count, found
+    elif name: return self.modelWrite(name) 
+    elif mid:  return new_record_count, models[mid]
+    else:      return new_record_count, models
+
+  def modelRead(self):
+    self.cursor.execute("""
+SELECT *
+FROM models;""")
+    models = [row[1] for row in self.cursor.fetchall()]
+    models.insert(0, None)  # avoid zero-based model list
+    return models
+
+  def modelWrite(self, name):
+    ''' create and return model ID
+    '''
+    self.cursor.execute("""
+INSERT INTO models (mid, model)
+VALUES (DEFAULT, %s)
+RETURNING mid;""", [name])
+    mid = self.cursor.fetchone()
+    new_record_count = 1
+    return new_record_count, mid[0]
+
+  def blocks(self, mid, blocks_v1=dict()):
+    ''' attempt to retrieve with mid else add new rows from db v1 values
+    
+        pos = {(11, 11): ('x', None)}
+    '''
+    blocks = self.blocksRead(mid)
+    if len(blocks):      return 0, blocks
+    elif len(blocks_v1): return self.blocksWrite(mid, blocks_v1)
+    else:                raise TypeError(f'missing either {mid} or {blocks_v1}')
+
+  def blocksRead(self, mid):
+    ''' read blocks by mid and return a dict
+    '''
+    blocks = dict()
+    self.cursor.execute("""
+SELECT *
+FROM blocks
+WHERE mid = %s;""", [mid])
+    for row in self.cursor.fetchall():
+      key = tuple(row[1])
+      blocks[key] = tuple(row[2:])
+    return blocks
+
+
+  def blocksWrite(self, mid, blocks):
+    ''' write block from same format as blocksRead returns
+    '''
+    new_record_count = 0
+
+    for position, celltop in blocks.items():
+      p0, p1    = position
+      pos_int   = f'{{ {p0}, {p1} }}'  # format postgres style
+      cell, top = celltop
+      self.cursor.execute("""
+INSERT INTO blocks (mid, position, cell, top)
+VALUES (%s, %s, %s, %s);""", (mid, pos_int, cell, top)
+      )
+      new_record_count += 1
+    return new_record_count, blocks
+
+  ''' make positions pretty for yaml
+  BLOCKSZ = tuple()
+  def setBlocksize(self, positions):
+    x = [p[0] for p in list(positions.keys())]
+    y = [p[1] for p in list(positions.keys())]
+    self.BLOCKSZ = (max(x) + 1, max(y) + 1)
+
+  def emptyBlock(self):
+    block = list(range(self.BLOCKSZ[1]))
+    for x in block:
+      row      = list(range(self.BLOCKSZ[0]))
+      block[x] = row
+    return block
+
+  def positionBlock(self, positions, top=False):
+    self.setBlocksize(positions)
+    i     = 1 if top else 0
+    block = self.emptyBlock()
+    for p in positions:
+      x, y = p
+      block[y][x] = positions[p][i]
+
+    if top: # does this model have any cells with top?
+      truth = list()
+      for row in block:
+        truth.append(all(t is None for t in row))
+      if truth.count(True) == len(block): block = None
+    return block
+  '''
+
+  def compass(self, mid, conf):
+    ''' retrieve compass entries by mid, optionally add new
+    '''
+    entries          = self.compassRead(mid)
+    if len(entries): return 0, entries
+    elif len(conf):  return self.compassWrite(mid, conf)
+    else:            raise TypeError('expected either mid or conf')
+
+  def compassWrite(self, mid, conf):
+    new_record_count = 0
+    entries = list()
+    for k, item in conf.items():
+      if k == 'C':    # pair is undefined
+        entry = list([mid, item[0], None, k]) 
+      else:
+        entry = list([mid, item[0][0], item[0][1], k]) # cell and pair
+
+      self.cursor.execute("""
+INSERT INTO compass (mid, cell, pair, facing)
+VALUES (%s, %s, %s, %s);""", entry
+      )
+      entries.append(entry)
+      new_record_count += 1
+    return new_record_count, entries
+
+  def compassRead(self, mid):
+    self.cursor.execute("""
+SELECT *
+FROM compass
+WHERE mid = %s;""", [mid])
+    entries = self.cursor.fetchall()
+    return entries
+
+  def pens(self, ver=0):
+    ''' return contents of pens table as list 
+        OR pens name if version given
+
+        TODO add method to create new pens
+    '''
+    self.cursor.execute("""
+SELECT ver, gplfile
+FROM pens;""")
+    pen = [row[1] for row in self.cursor.fetchall()]
+    pen.insert(0, None)  # avoid zero-based list
+    #self.pp.pprint(pen)
+    if ver: return pen[ver]
+    else:   return pen
+
+'''
+the
+end
+'''
---
+++ t/modeldata2.py
@@ -0,0 +1,63 @@
+import unittest
+import pprint
+from model.data2 import ModelData2
+
+class Test(unittest.TestCase):
+
+  def setUp(self):
+    self.m   = ModelData2() 
+    self.pp  = pprint.PrettyPrinter(indent=2)
+    self.mid = 2 # mambo
+
+  def test_a(self):
+    ''' read model
+    '''
+    models = self.m.modelRead()
+    self.assertTrue(len(models))
+
+  def test_b(self):
+    ''' get model id
+    '''
+    nrc, mid = self.m.model('mambo')
+    self.assertTrue(mid)
+
+  def test_c(self):
+    ''' read blocks
+    '''
+    blocks = self.m.blocksRead(self.mid)
+    #self.pp.pprint(blocks)
+    self.assertEqual(144, len(blocks))
+
+  def test_d(self):
+    ''' write 1 position in mambo block
+
+DELETE from blocks where cell = 'z';
+    '''
+    blocks = {(99, 0): ('z', None)}
+
+    nrc, blocks = self.m.blocksWrite(self.mid, blocks)
+    self.pp.pprint(blocks)
+    self.assertTrue(nrc)
+
+  def test_e(self):
+    ''' get default scale for model
+
+[(2, 'z', None, 'C')] mid cell pair facing
+not yet done the conf, this is still raw dbrows format
+    '''
+    conf = self.m.compassRead(self.mid)
+    self.assertTrue(len(conf))
+
+
+  def test_f(self):
+    ''' write conf and then 
+
+DELETE FROM compass WHERE mid=2 AND cell='z';
+    '''
+    conf      = {'C': ['z']}
+    nrc, conf = self.m.compassWrite(self.mid, conf)
+
+  def test_g(self):
+    ver  = 1 # uniball
+    name = self.m.pens(ver)
+    self.assertEqual('uniball', name)
