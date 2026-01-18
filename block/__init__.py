b9881f23d13ec5335d315f5b726b5fdb1aa546ab
snow6oy <snow6oy@gmail.com>
2026-01-18 22:27:53 +0100

    remove Db2 as refactor all done :-)

--- block/__init__.py
+++ block/__init__.py
@@ -2,7 +2,6 @@ from cell import Layer
 from .validator import InputValidator
 from .make import Make
 from .data import Views, BlockData
-from .dbmigrator import Db2
 from .tmpfile import TmpFile
 from .styles import Styles
 from .palette import PaletteMaker
