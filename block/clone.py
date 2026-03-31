bcfca820fdfccf5ba500a8392bc15cdd9b3e0827
snow6oy <snow6oy@gmail.com>
2026-03-31 18:26:12 +0200

    broke model clone in two

---
+++ block/clone.py
@@ -0,0 +1,69 @@
+import pprint
+from block import PaletteMaker, TmpFile, BlockData
+from cell import CellData
+
+class Clone(BlockData):
+  ''' pull from db to create tmp/MODEL.txt from rinkid
+  '''
+  VERBOSE = False
+  pp      = pprint.PrettyPrinter(indent=2)
+  cd      = CellData()
+  tf      = TmpFile()
+  pmk     = PaletteMaker()
+
+  def palSwap(self, rinkid, ver):
+    ''' transform a rink to use a new palette
+        e.g sharpie 180f1989f54ff03291ec31e164f2a79f
+
+    '''
+    mid, *extra = self.rinks(rinkid)     # lookup model name
+    rinkset  = set()                     
+    celldata = self.cd.layers(rinkid)    # get colors used by rink
+    for label, cell in celldata.items():
+      for z, row in enumerate(cell):
+        if not len(row): continue
+        #print(f'{label} {z} {row[3]}')
+        rinkset.add(row[3])
+    colors   = self.colors(ver)
+    colors   = set([c[0] for c in colors])  # strip out penam
+    colors   = self.pmk.setLookUp(colors)   # RGB to search the new colour
+    swp, out = self.pmk.swapColors(rinkset, colors)
+   
+    for label, cell in celldata.items():
+      layers = list()
+      for z in cell:
+        if not len(z): continue
+        old_stroke   = z[3]
+        new_layer    = list(z)
+        new_layer[3] = swp[old_stroke]
+        layers.append(tuple(new_layer))
+      celldata[label] = layers  # update db colors 
+    celldata = self.cd.txDbv3Yaml(celldata)
+    #self.pp.pprint(celldata)
+    return mid, celldata, out
+
+  def palette(self, ver):
+    ''' get pen colors
+    '''
+    return self.colors(ver=ver)
+
+  def rink(self, rinkid):  # we want celldata as YAML
+    ''' rink and celldata
+    '''
+    mid, ver, clen, factor, *dates = self.rinks(rinkid)
+    celldata = self.cd.layers(rinkid)
+    celldata = self.cd.txDbv3Yaml(celldata)
+    return mid, ver, celldata
+
+  def writeConf(self, model, ver, rinkid, celldata):
+    self.tf.setVersion(ver)
+    self.tf.writePretty(model, celldata, rinkid=rinkid)
+    return f"""
+cloning {model}
+with {len(celldata)} cells
+"""
+
+'''
+the
+end
+'''
