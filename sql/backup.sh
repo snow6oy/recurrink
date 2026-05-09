a2cefdad147bcb65f4a10f56ed1f59cefbb23142
Gavin Johnson <4960142+snow6oy@users.noreply.github.com>
2026-05-09 21:08:13 +0200

    using clean on backup

--- sql/backup.sh
+++ sql/backup.sh
@@ -1,12 +1,11 @@
 # backup data to cloud
-# to start pCloud on ubuntu
-# pcloudcc -u xyz@example.com -p 
-
-# before restoring 
-# cd sql
-# psql -d recurrink2
-# \i ddl_v2.sql
 DUMPFILE=recurrink2.sql
+
+if [ "$#" -ne 1 ]
+then
+  echo "usage: $0 dump|restore"
+  exit
+fi
 
 if [[ "$OSTYPE" == "darwin"* ]]
 then
@@ -16,18 +15,10 @@ else
 fi
 pwd
 
-
-if [ "$#" -ne 1 ]
-then
-  echo "usage: $0 dump|restore"
-  exit
-fi
-
 if [ "$1" = 'dump' ]
 then
   echo "dumping ${DUMPFILE}"
-  # pg_dump -d recurrink2 --column-inserts --data-only > ${DUMPFILE}
-  pg_dump -d recurrink2 > ${DUMPFILE}
+  pg_dump --clean -d recurrink2 > ${DUMPFILE}
 elif [ "$1" = 'restore' ]
 then
   echo "restoring ${DUMPFILE}"
